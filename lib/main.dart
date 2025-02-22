181637c39d3fc6e1451682e250aee93dfc7ea748
A3ToF <mohamedandmostafa77@gmail.com>
2025-02-22 11:03:51 +0200

    configure Firebase integration and add necessary dependencies for multi-platform support

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ import 'package:ecommerce/featuers/splash/presentation/views/splash_view.dart';
 import 'package:ecommerce/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prefs.init();
   runApp(const FruitHub());
 }
